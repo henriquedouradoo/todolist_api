526c67f9e04e87edb2d9bc63cf94498b411f3831
Henrique Dourado Domingos <henriquedsenai@gmail.com>
2024-12-28 22:15:24 -0300

    adicionando getters e setters com lombok (simplificando codigo)

--- src/main/java/br/com/henriquedouradoo/todolist/user/UserModel.java
+++ src/main/java/br/com/henriquedouradoo/todolist/user/UserModel.java
@@ -1,32 +1,13 @@
 package br.com.henriquedouradoo.todolist.user;
 
+import lombok.Data;
+
+// Getters e Setters usando o lombok
+@Data
 public class UserModel {
     
     private String name;
     private String username;
     private String password;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
